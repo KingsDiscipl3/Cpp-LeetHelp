70670bba7a52763655d9715f93ccdd0074eb6ee6
KingsDiscipl3 <65213015+KingsDiscipl3@users.noreply.github.com>
2021-06-09 10:21:59 -0700

    Update easy_try.cpp

--- easy_try.cpp
+++ easy_try.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-/* Leetcode Problem -Easy- Reverse Integer +/
-class Solution {
+// Leetcode Problem -Easy- Reverse Integer
+class ReverseIntegerSolution001 {
 public:
     int reverse(int x) {
         int rev = 0;
@@ -18,3 +18,57 @@ public:
         return rev;
     }
 };
+
+
+//Leetcode Problem -Easy- Two Sums -Bruteforce Example
+class TwoSumsSolution001{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ret;
+        int size = nums.size();
+        for(int i = 0; i < size - 1; i++) 
+        {
+            for(int j = i+1; j < size; j++) 
+            {
+                if(nums[0] + nums[1] == target) 
+                {
+                    ret.push_back(i);
+                    ret.push_back(j);
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }
+};
+
+
+//Leetcode Problem -Easy- Two Sums -Maps Example
+class TwoSumsSolution002 {
+public:
+     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ret;
+        int size = nums.size();
+        int diff;
+        unordered_map<int, int>m;
+        for(int i = 0; i< size; i++)
+        {
+            diff = target - nums[i];
+            if(m.find(diff) != m.end() && m.find(diff)->second != i) 
+            {
+                ret.push_back(i);
+                ret.push_back(m.find(diff)->second);
+                return ret;
+            }
+            m[nums[i]] = i;
+        }
+        return ret;
+    }
+};
+
+    
+    
+    
+    
+    
+    
