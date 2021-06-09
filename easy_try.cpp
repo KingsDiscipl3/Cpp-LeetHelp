52f8afe62c45dd268ade4dca3db46600c0d3cef5
KingsDiscipl3 <65213015+KingsDiscipl3@users.noreply.github.com>
2021-06-08 22:21:59 -0700

    Update easy_try.cpp

--- easy_try.cpp
+++ easy_try.cpp
@@ -2,17 +2,19 @@
 #include "Objects/box.h"
 
 using namespace std;
+
 /* Leetcode Problem -Easy- Reverse Integer +/
-class ReverseIntegerSolution {
-    public int reverse(int x) {
+class Solution {
+public:
+    int reverse(int x) {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
         }
         return rev;
     }
-}
+};
